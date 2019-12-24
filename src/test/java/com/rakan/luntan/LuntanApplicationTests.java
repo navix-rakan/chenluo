475dee742e95ca9903a66ca80d3c8fc05817c1c3
Rakan <270981565@qq.com>
2019-12-24 10:25:01 +0800

    change

---
+++ src/test/java/com/rakan/luntan/LuntanApplicationTests.java
@@ -0,0 +1,13 @@
+package com.rakan.luntan;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LuntanApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
